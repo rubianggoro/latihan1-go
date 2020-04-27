e95af32c3370d17a0f089a7ef08afd667e65ad2a
rubianggoro <rubi.anggoro199@gmail.com>
2020-04-27 15:39:05 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	// SOAL 1
+	dollar := 14.0
+	rupiah := 15515.90
+
+	x := dollar * rupiah
+	
+	fmt.Println("$", dollar, "sama dengan", x, "rupiah" )
+
+
+	// SOAL 2
+	vhi := 3.14
+	r := 15.0
+
+	luasLingkaran := vhi * (r * r)
+	fmt.Println("Luas Lingkaran",luasLingkaran, "cm")
+
+}
